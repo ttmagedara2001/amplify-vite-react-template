cc4e408d382e7349e533173002eda9d72282d502
ttmagedara2001 <thulanimagedara@gmail.com>
2026-06-25 09:57:16 +0530

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -17,12 +17,12 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: 'userPool',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
-    },
-        defaultAuthorizationMode: 'userPool',
+    }, 
+    
   },
 });
 
